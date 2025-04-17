2f1d123ba34b483dbe81d18dcf0712cca1e26d0c
AeroAstroid <aeroastroid@gmail.com>
2025-04-17 01:37:50 -0300

    Simplificar o script do A*

--- algoritmos/A_estrela.py
+++ algoritmos/A_estrela.py
@@ -3,43 +3,43 @@ import math
 
 from dados.cidades import coordenadas
 
-def heuristica_A_estrela(cidade, meta, coords):
+def heuristica_A_estrela(cidade_atual, cidade_meta):
     # h(n) do algoritmo: distância em linha reta entre a cidade atual e a meta
 
-    x1, y1 = coords[cidade]
-    x2, y2 = coords[meta]
+    x1, y1 = coordenadas[cidade_atual]
+    x2, y2 = coordenadas[cidade_meta]
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
-def A_estrela(grafo, inicio, meta):
+def A_estrela(grafo, cidade_inicial, cidade_meta):
     # Base do algoritmo A*
 
     open_set = []
-    h_inicial = heuristica_A_estrela(inicio, meta, coordenadas)
-    heapq.heappush(open_set, (h_inicial, inicio, 0))
+    h_inicial = heuristica_A_estrela(cidade_inicial, cidade_meta, coordenadas)
+    heapq.heappush(open_set, (h_inicial, cidade_inicial, 0))
     
     came_from = {}   # Para reconstruir o caminho
-    g_score = {inicio: 0}  # Custo real acumulado até cada cidade
+    g_score = {cidade_inicial: 0}  # Custo real acumulado até cada cidade
 
-    print(f"Inicializando A* com nó de partida: {inicio}")
+    print(f"Inicializando A* com nó de partida: {cidade_inicial}")
     
     while open_set:
         f_atual, atual, g_atual = heapq.heappop(open_set)
 
         # Verifica se já chegou na meta
-        if atual == meta:
+        if atual == cidade_meta:
             caminho = []
             while atual in came_from:
                 caminho.append(atual)
                 atual = came_from[atual]
-            caminho.append(inicio)
+            caminho.append(cidade_inicial)
             caminho.reverse()
             print("\nMeta alcançada!")
-            return caminho, g_score[meta]
+            return caminho, g_score[cidade_meta]
         
         # Explora os vizinhos
         for vizinho, custo in grafo.get(atual, []):
             novo_g = g_atual + custo
-            h_vizinho = heuristica_A_estrela(vizinho, meta, coordenadas)
+            h_vizinho = heuristica_A_estrela(vizinho, cidade_meta, coordenadas)
             f_vizinho = novo_g + h_vizinho
 
             if vizinho not in g_score or novo_g < g_score[vizinho]:
