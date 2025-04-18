b1e0412e4f9f506daf43586f493e95f140e66ddc
Amigao <pedrofrancoso@usp.br>
2025-04-18 13:25:20 -0300

    feat: mais cidades e arestas adicionadas ao grafo (valores não foram calculados idealmente, só estimados)

--- dados/cidades.py
+++ dados/cidades.py
@@ -8,7 +8,18 @@ distancias_cidades = [
     ("Rio de Janeiro", "Vitória", 517),
     ("São José do Rio Preto", "Uberlândia", 318),
     ("Uberlândia", "Belo Horizonte", 240),
+    ("São Paulo", "Campinas", 99),
+    ("Campinas", "Ribeirão Preto", 210),
+    ("São Paulo", "Sorocaba", 102),
+    ("Presidente Prudente", "São José do Rio Preto", 263),
+    ("Presidente Prudente", "Ribeirão Preto", 325),
+    ("Presidente Prudente", "Uberlândia", 368),
+    ("Campinas", "Sorocaba", 96),
+    ("Sorocaba", "Barra Mansa", 256),
+    ("Campinas", "Barra Mansa", 275),
+    ("Belo Horizonte", "Vitória", 524),
 ]
+
 
 # Construindo o grafo (dicionário de vizinhança) a partir da lista de arestas.
 grafo_distancias = {}
@@ -22,12 +33,17 @@ for origem, destino, custo in distancias_cidades:
 
 # Coordenadas de cada cidade
 coordenadas = {
-    "São José do Rio Preto": (64,95),
+    "São José do Rio Preto": (64, 95),
     "Ribeirão Preto": (150, 80),
     "São Paulo": (200, 50),
+    "Campinas": (180, 60),
+    "Sorocaba": (180, 40),
+    "Presidente Prudente": (10, 75),
     "Barra Mansa": (300, 20),
     "Rio de Janeiro": (400, 100),
     "Vitória": (500, 150),
-    "Uberlândia": (179, 130),      
-    "Belo Horizonte": (300, 120)     
+    "Uberlândia": (179, 130),
+    "Belo Horizonte": (300, 120),
 }
+
+
